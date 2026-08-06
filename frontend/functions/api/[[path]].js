e8894f3bd0b1e1609d1a3dfd3b3c002e72a4ef2c
subh-ghosh <subartaghosh6@gmail.com>
2026-08-06 14:47:54 +0530

    fix: template string formatting in Cloudflare Pages API proxy function

--- frontend/functions/api/[[path]].js
+++ frontend/functions/api/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const targetPath = url.pathname.replace(/^\/api/, '') || '/';
-  const backendUrl = http://100.26.51.167;
+  const backendUrl = 'http://100.26.51.167' + targetPath + url.search;
 
   const modifiedHeaders = new Headers(context.request.headers);
   modifiedHeaders.set('Host', '100.26.51.167');
